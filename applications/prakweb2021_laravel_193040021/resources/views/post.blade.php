9ca504295063a9f2246816abb44b3788b184676b
Yunisetyawati30 <60532944+Yunisetyawati30@users.noreply.github.com>
2021-10-14 12:42:20 -0700

    Menyelesaikan video ke 8 untuk mempelajari Post Category dan Eloquent Relationship

--- applications/prakweb2021_laravel_193040021/resources/views/post.blade.php
+++ applications/prakweb2021_laravel_193040021/resources/views/post.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.main')
 
 @section('container')
-<article>
-  <h2>{{ $post->title }}</h2>
-  <h5>{{ $post->author }}</h5>
+  <h1 class="mb-5">{{ $post->title }}</h1>
+
+  <p>By. Yuni Setyawati in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
   {!! $post->body !!}
-</article>
+
 <a href="/blog">Back to Posts</a>
 @endsection
