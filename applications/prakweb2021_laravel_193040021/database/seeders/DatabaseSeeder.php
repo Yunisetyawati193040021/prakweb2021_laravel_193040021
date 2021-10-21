bb53b3c33aaa9096f6104baaededdd4b287043f7
Yunisetyawati30 <60532944+Yunisetyawati30@users.noreply.github.com>
2021-10-21 08:00:25 -0700

    Menyelesaikan video ke 9 untuk mempelajari Database Seeder

--- applications/prakweb2021_laravel_193040021/database/seeders/DatabaseSeeder.php
+++ applications/prakweb2021_laravel_193040021/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,63 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Yuni Setyawati',
+            'email' => 'yuni@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Yanida Nur Nabila',
+            'email' => 'yanida@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum consectetur dolore laboriosam minima molestiae vitae perspiciatis, consequatur quo, veniam illo ullam mollitia accusantium ipsum modi tenetur reprehenderit explicabo alias.',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum consectetur dolore laboriosam minima molestiae vitae perspiciatis, consequatur quo, veniam illo ullam mollitia accusantium ipsum modi tenetur reprehenderit explicabo alias. Sunt quos molestias voluptatem voluptatibus, enim deserunt perferendis, tempora commodi qui quod delectus. Pariatur officiis nihil quasi ratione sint molestiae quibusdam quia repellat recusandae ipsum aperiam porro rem repellendus veritatis ad officia asperiores fuga, corporis eum temporibus, saepe dolorem eaque ipsam! Libero eos, exercitationem nam unde officia voluptatibus autem expedita distinctio. Magnam eos voluptas earum, deserunt consequatur alias veniam esse. Asperiores vel laudantium error ducimus mollitia, dolorem tenetur porro aliquid eius!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum consectetur dolore laboriosam minima molestiae vitae perspiciatis, consequatur quo, veniam illo ullam mollitia accusantium ipsum modi tenetur reprehenderit explicabo alias.',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum consectetur dolore laboriosam minima molestiae vitae perspiciatis, consequatur quo, veniam illo ullam mollitia accusantium ipsum modi tenetur reprehenderit explicabo alias. Sunt quos molestias voluptatem voluptatibus, enim deserunt perferendis, tempora commodi qui quod delectus. Pariatur officiis nihil quasi ratione sint molestiae quibusdam quia repellat recusandae ipsum aperiam porro rem repellendus veritatis ad officia asperiores fuga, corporis eum temporibus, saepe dolorem eaque ipsam! Libero eos, exercitationem nam unde officia voluptatibus autem expedita distinctio. Magnam eos voluptas earum, deserunt consequatur alias veniam esse. Asperiores vel laudantium error ducimus mollitia, dolorem tenetur porro aliquid eius!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum consectetur dolore laboriosam minima molestiae vitae perspiciatis, consequatur quo, veniam illo ullam mollitia accusantium ipsum modi tenetur reprehenderit explicabo alias.',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum consectetur dolore laboriosam minima molestiae vitae perspiciatis, consequatur quo, veniam illo ullam mollitia accusantium ipsum modi tenetur reprehenderit explicabo alias. Sunt quos molestias voluptatem voluptatibus, enim deserunt perferendis, tempora commodi qui quod delectus. Pariatur officiis nihil quasi ratione sint molestiae quibusdam quia repellat recusandae ipsum aperiam porro rem repellendus veritatis ad officia asperiores fuga, corporis eum temporibus, saepe dolorem eaque ipsam! Libero eos, exercitationem nam unde officia voluptatibus autem expedita distinctio. Magnam eos voluptas earum, deserunt consequatur alias veniam esse. Asperiores vel laudantium error ducimus mollitia, dolorem tenetur porro aliquid eius!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum consectetur dolore laboriosam minima molestiae vitae perspiciatis, consequatur quo, veniam illo ullam mollitia accusantium ipsum modi tenetur reprehenderit explicabo alias.',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum consectetur dolore laboriosam minima molestiae vitae perspiciatis, consequatur quo, veniam illo ullam mollitia accusantium ipsum modi tenetur reprehenderit explicabo alias. Sunt quos molestias voluptatem voluptatibus, enim deserunt perferendis, tempora commodi qui quod delectus. Pariatur officiis nihil quasi ratione sint molestiae quibusdam quia repellat recusandae ipsum aperiam porro rem repellendus veritatis ad officia asperiores fuga, corporis eum temporibus, saepe dolorem eaque ipsam! Libero eos, exercitationem nam unde officia voluptatibus autem expedita distinctio. Magnam eos voluptas earum, deserunt consequatur alias veniam esse. Asperiores vel laudantium error ducimus mollitia, dolorem tenetur porro aliquid eius!',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
