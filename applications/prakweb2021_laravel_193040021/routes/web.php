46d8381ec82975c5d417ef0829767ecb402457d2
Yunisetyawati30 <60532944+Yunisetyawati30@users.noreply.github.com>
2021-10-28 11:09:09 -0700

    Menyelesaikan video ke 13 untuk mempelajari Searching & Pagination

--- applications/prakweb2021_laravel_193040021/routes/web.php
+++ applications/prakweb2021_laravel_193040021/routes/web.php
@@ -19,22 +19,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => 'home'
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
-        "title" => "About",
-        "name" => "Yuni Setyawati",
-        "email" => "yunisetyawati@unpas.ac.id",
-        "image" => "yuni.jpg"
+        'title' => 'About',
+        'active' => 'about',
+        'name' => 'Yuni Setyawati',
+        'email' => 'yunisetyawati@unpas.ac.id',
+        'image' => 'yuni.jpg'
     ]);
 });
 
 
 
-Route::get('/blog', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories', function() {
@@ -42,20 +44,5 @@ Route::get('/categories', function() {
         'title' => 'Post Categories',
         'active' => 'categories',
         'categories' => Category::all()
-    ]);
-});
-
-Route::get('/categories/{category:slug}', function(Category $category) {
-    return view('posts', [
-        'title' => "Post by Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
-
-Route::get('/authors/{author:username}', function(User $author) {
-    return view('posts', [
-        'title' => "Post by Author : $author->name",
-        'posts' => $author->posts->load('category', 'author')
     ]);
 });
