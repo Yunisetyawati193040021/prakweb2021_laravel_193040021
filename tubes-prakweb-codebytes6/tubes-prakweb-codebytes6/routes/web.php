6c6a0f49669005d5a6c3d70fe25fb53b452ba134
Yunisetyawati30 <60532944+Yunisetyawati30@users.noreply.github.com>
2021-12-16 06:35:21 -0800

    Menyelesaikan Design UI

---
+++ tubes-prakweb-codebytes6/tubes-prakweb-codebytes6/routes/web.php
@@ -0,0 +1,74 @@
+<?php
+
+
+use App\Models\Category;
+use PhpParser\Node\Stmt\If_;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+use App\Http\Controllers\DashboardPostController;
+use App\Http\Controllers\AdminCategoryController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "home",
+        "active" => 'home'
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "about",
+        "active" => 'about',
+        "name" => "Muhammad Viqri Febriana",
+        "email" => "193040041@mail.unpas.ac.id",
+        "image" => "gambar.jpg"
+    ]);
+});
+
+
+Route::get('/post', [PostController::class, 'index']);
+// halaman single post
+route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+
+route::get('/categories', function () {
+    return view('categories', [
+        'title' => 'Post Categories',
+        "active" => 'categories',
+        'categories' => Category::all()
+    ]);
+});
+
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth');
+
+Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
